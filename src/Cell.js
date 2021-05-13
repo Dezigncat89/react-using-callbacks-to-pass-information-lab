2dd0189991e9acc105fb75e9e82d454b3374b756
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-12 19:26:20 -0700

    Update Cell.js

--- src/Cell.js
+++ src/Cell.js
@@ -17,3 +17,31 @@ export default class Cell extends Component {
   }
   
 }
+
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.color
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.selectedColor
+    })
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick} className="cell"
+           style={{backgroundColor: this.state.color}}
+      >
+      </div>
+    )
+  }
+
+}
